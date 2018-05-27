0e681937dd2f1cb8ebe53c2dce817d80086075f9
eric86103 <eric86103@hotmail.com>
2018-05-27 21:14:04 +0800

    tb revise

--- nn_wraper/tb_sng.sv
+++ nn_wraper/tb_sng.sv
@@ -4,12 +4,13 @@ module tb;
 	localparam HCLK = CLK/2;
 	logic clk, start, rst, stop;
    logic [3:0] x;
+   logic [7:0] streams;
    logic bit_stream;
    initial begin
       clk = 0;
-      x = 6;
    end
 	always #HCLK clk = ~clk;
+
 
    SNG sng(
       .i_clk_sng(clk),
@@ -26,14 +27,20 @@ module tb;
 		rst = 1;
 		#(2*CLK)
 		rst = 0;
-      start = 1;
       #CLK
-      start = 0;
-		#(100*CLK)
-      stop = 1;
-      #CLK
-      stop = 0;
-
+      for(int i = 0; i < 15; ++i) begin
+         start <= 1;
+         stop <= 0;
+         x <= 0;
+         $display("Binary number is : \"%2d\"", i);
+         for(int j = 0; j < 8; ++j) begin
+            start <= 0;
+            @(posedge clk)
+            streams[i] = bit_stream
+         end
+         $display("Stochastic number is : \"%8x\"", streams);
+         stop <= 1;
+      end
 		$finish;
    end
    endmodule
